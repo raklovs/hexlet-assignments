4636a8965742d2e277acbbf28399c31201a7f229
Sergey Raklov <ra1976@inbox.ru>
2022-10-05 09:00:52 +0300

    update .gitignore

--- java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
+++ java-oop-ru/classes-and-objects/src/main/java/exercise/Segment.java
@@ -2,12 +2,8 @@ package exercise;
 
 // BEGIN
 public class Segment {
-
     private final Point point1;
     private final Point point2;
-
-    int midPointX;
-    int midPointY;
 
     public Segment(Point point1, Point point2) {
         this.point1 = point1;
@@ -21,28 +17,19 @@ public class Segment {
     }
 
     public Point getMidPoint() {
-        midPointX = (getBeginPoint().getX()) + (getEndPoint().getX());
+        int midPointX = (getBeginPoint().getX()) + (getEndPoint().getX());
         if (midPointX > 0) {
             midPointX -= 1;
         } else {
             midPointX += 1;
         }
-        midPointY = (getBeginPoint().getY()) + (getEndPoint().getY());
+        int midPointY = (getBeginPoint().getY()) + (getEndPoint().getY());
         if (midPointY > 0) {
             midPointY -= 1;
         } else {
             midPointY += 1;
         }
         return new Point(midPointX, midPointY);
-    }
-
-    public static void main(String[] args) {
-        Point point1 = new Point(4, 3);
-        Point point2 = new Point(1, 1);
-        Segment segment = new Segment(point1, point2);
-        Point midPoint = segment.getMidPoint();
-        System.out.println(midPoint.getX()); // 4
-        System.out.println(midPoint.getY()); //  3
     }
 }
 // END
---
+++ java-oop-ru/interfaces/src/main/java/exercise/App.java
@@ -0,0 +1,8 @@
+package exercise;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+// BEGIN
+
+// END
