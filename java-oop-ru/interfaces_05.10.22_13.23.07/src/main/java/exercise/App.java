d67a55e0ac937e57636382fe080d1237d245673a
Sergey Raklov <ra1976@inbox.ru>
2022-11-08 19:04:26 +0300

    update App.java

--- java-oop-ru/interfaces_05.10.22_13.23.07/src/main/java/exercise/App.java
+++ java-oop-ru/interfaces_05.10.22_13.23.07/src/main/java/exercise/App.java
@@ -1,21 +1,16 @@
 package exercise;
 
-import java.util.ArrayList;
 import java.util.List;
 
 // BEGIN
 public class App {
 
     public static List<String> buildAppartmentsList(List<Home> appartments, int n) {
-        List<String> result = new ArrayList<>();
-        appartments.sort(Home::compareTo);
-        if (n <= appartments.size()) {
-            for (int i = 0; i < n; i++) {
-                result.add(String.valueOf(appartments.get(i)));
-            }
-            return result;
-        }
-        else return new ArrayList<>();
+        return appartments.stream()
+                .sorted(Home::compareTo)
+                .limit(n)
+                .map(Home::toString)
+                .toList();
     }
 }
 // END
