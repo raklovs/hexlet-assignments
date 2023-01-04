60c18418406146525d0028737dc1106f6be83658
Sergey Raklov <ra1976@inbox.ru>
2023-01-04 11:59:36 +0300

    add homework

--- java-collections-ru/advanced-streams/src/main/java/exercise/App.java
+++ java-collections-ru/advanced-streams/src/main/java/exercise/App.java
@@ -8,9 +8,11 @@ public class App {
     public static String getForwardedVariables(String content) {
         String[] result = content.split("\n");
         return Arrays.stream(result)
-                .filter(environment -> environment.contains("environment="))
-                .map(environment -> environment.replace("environment=", "").replaceAll("\"", ""))
-                .flatMap(forvarded -> Arrays.stream(forvarded.split(",")).filter(a -> a.contains("X_FORWARDED_")))
+                .filter(environment -> environment.startsWith("environment="))
+                .map(environment -> environment.replaceAll("environment=", "")
+                        .replaceAll("\"", ""))
+                .flatMap(forvarded -> Arrays.stream(forvarded.split(","))
+                        .filter(a -> a.contains("X_FORWARDED_")))
                 .map(forvarded -> forvarded.replaceAll("X_FORWARDED_", ""))
                 .collect(Collectors.joining(","));
     }
