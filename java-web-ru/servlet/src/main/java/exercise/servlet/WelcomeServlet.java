ff3290ef8cc05d8e0abad1597eb5ef77929cefd4
@hexlet/cli <support@hexlet.io>
2023-01-04 14:49:24 +0300

    submit java-web-ru/servlet

---
+++ java-web-ru/servlet/src/main/java/exercise/servlet/WelcomeServlet.java
@@ -0,0 +1,22 @@
+package exercise.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+// BEGIN
+public class WelcomeServlet extends HttpServlet {
+
+    @Override
+    public void doGet(HttpServletRequest request,
+                      HttpServletResponse response)
+            throws IOException, ServletException {
+
+        PrintWriter out = response.getWriter();
+        out.println("Hello, Hexlet!");
+    }
+}
+// END
