4745909e27381e1c4238ef6a86e3e612ea64e91f
@hexlet/cli <support@hexlet.io>
2023-02-01 20:02:35 +0300

    submit java-spring-ru/spring-tests

--- java-spring-ru/spring-tests/src/main/java/exercise/controller/WelcomeController.java
+++ java-spring-ru/spring-tests/src/main/java/exercise/controller/WelcomeController.java
@@ -1,4 +1,4 @@
-package exercise;
+package exercise.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- java-spring-ru/spring-tests/src/test/java/exercise/AppTest.java
+++ java-spring-ru/spring-tests/src/test/java/exercise/AppTest.java
@@ -12,8 +12,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 @SpringBootTest
 // При тестировании можно вообще не запускать сервер
